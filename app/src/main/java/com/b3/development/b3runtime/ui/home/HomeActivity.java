ac55b80e24267f2b7f93710c77d933c6491650a0
William Jelvin <william.jelvin@gmail.com>
2019-10-28 14:26:54 +0100

    moved signin to separate class, refactored some code

--- app/src/main/java/com/b3/development/b3runtime/ui/home/HomeActivity.java
+++ app/src/main/java/com/b3/development/b3runtime/ui/home/HomeActivity.java
@@ -2,34 +2,23 @@ package com.b3.development.b3runtime.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.b3.development.b3runtime.R;
+import com.b3.development.b3runtime.ui.signin.SignInActivity;
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     public static final String TAG = HomeActivity.class.getSimpleName();
-    private static final int RC_SIGN_IN = 123;
 
     private HomeViewModel homeViewModel;
-
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,79 +30,18 @@ public class HomeActivity extends AppCompatActivity {
                 .get(HomeViewModel.class);
 
         setContentView(R.layout.activity_home);
-
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                //Try getting logged in user
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-
-                if (user != null) {
-                    //logged in, toast a welcome
-                    Toast.makeText(HomeActivity.this, "Welcome " + user.getDisplayName() + ".", Toast.LENGTH_SHORT).show();
-                } else {
-                    //not logged in, create sign in intent
-                    createSigninIntent();
-                }
-            }
-        };
-
     }
-
-    public void createSigninIntent() {
-        //creates login providers
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build());
-
-        //create and launch intent
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .setIsSmartLockEnabled(false)
-                        .setLogo(R.drawable.b3logo_yellow_signin)
-                        .setTheme(R.style.SignInTheme)
-                        .build(),
-                RC_SIGN_IN);
-    }
-
 
     public void signOut(View view) {
         AuthUI.getInstance()
                 .signOut(this)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     public void onComplete(@NonNull Task<Void> task) {
-                        //todo: add some confirmation for user?
+                        Intent intent = new Intent(HomeActivity.this, SignInActivity.class);
+                        startActivity(intent);
+                        finish();
                     }
                 });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            if (resultCode == RESULT_OK) {
-                //successfully signed in
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                Toast.makeText(HomeActivity.this, "You are signed in! " + user.getDisplayName(), Toast.LENGTH_SHORT).show();
-
-            } else {
-                //sign in failed
-                if (response == null) {
-                    //user canceled login
-                    Log.d(TAG, "User canceled login");
-                } else {
-                    //some error happened
-                    Log.d(TAG, "Error code: " + response.getError().getErrorCode());
-                }
-            }
-        }
     }
 
     @Override
@@ -124,15 +52,11 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mAuthStateListener != null) {
-            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
-        }
     }
 
     @Override
