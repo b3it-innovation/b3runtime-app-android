a1e7d2dbe3a910e3a92e13ec636917334cf40b2f
William Jelvin <william.jelvin@gmail.com>
2019-10-31 09:37:42 +0100

    remodeled change username

--- app/src/main/java/com/b3/development/b3runtime/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/b3/development/b3runtime/ui/profile/ProfileFragment.java
@@ -1,22 +1,25 @@
 package com.b3.development.b3runtime.ui.profile;
 
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
+import android.text.InputFilter;
+import android.text.InputType;
+import android.text.Spanned;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
 
 import com.b3.development.b3runtime.R;
-import com.b3.development.b3runtime.ui.signin.SignInActivity;
-import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,7 +28,7 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.auth.UserInfo;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -63,9 +66,9 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        Button btnResetPassword = view.findViewById(R.id.btn_reset_password);
-        Button btnDeleteAccount = view.findViewById(R.id.btn_delete_account);
+        View fragView = inflater.inflate(R.layout.fragment_profile, container, false);
+        Button btnResetPassword = fragView.findViewById(R.id.btn_reset_password);
+        Button btnChangeName = fragView.findViewById(R.id.btnEditName);
 
         btnResetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,15 +77,15 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        btnDeleteAccount.setOnClickListener(new View.OnClickListener() {
+        btnChangeName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                deleteUser(view);
+                changeName(fragView);
             }
         });
 
-        drawProfile(view);
-        return view;
+        drawProfile(fragView);
+        return fragView;
     }
 
     private void drawProfile(View view) {
@@ -90,32 +93,75 @@ public class ProfileFragment extends Fragment {
         String userName = user.getDisplayName();
         String email = user.getEmail();
 
-        TextView name = view.findViewById(R.id.textViewName);
+        TextView name = view.findViewById(R.id.editTextName);
         TextView mail = view.findViewById(R.id.textViewMail);
 
         name.setText(userName);
         mail.setText(email);
     }
 
-    public void deleteUser(View view) {
-        reauthenticate();
-        user.delete()
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
+    private void changeName(View view) {
+
+        //get old name
+        TextView name = view.findViewById(R.id.editTextName);
+        String newName = name.getText().toString();
+
+        //create dialog, insert old name as placeholder
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Enter new name");
+        final EditText input = new EditText(getContext());
+
+        //filters for text
+        InputFilter[] filterArray = new InputFilter[2];
+        filterArray[0] = new InputFilter.LengthFilter(20);
+        InputFilter filter = new InputFilter() {
+            public CharSequence filter(CharSequence source, int start, int end,
+                                       Spanned dest, int dstart, int dend) {
+                for (int i = start; i < end; i++) {
+                    if (!Character.isLetterOrDigit(source.charAt(i))) {
+                        return "";
+                    }
+                }
+                return null;
+            }
+        };
+        filterArray[1] = filter;
+        input.setFilters(filterArray);
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_CLASS_TEXT);
+        input.setText(newName);
+        builder.setView(input);
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //take the new name entered and set it
+                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(input.getText().toString())
+                        .build();
+                user.updateProfile(profileUpdates).addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            Log.d(TAG, "User account deleted.");
-                            Intent intent = new Intent(getContext(), SignInActivity.class);
-                            // clean up back stack
-                            for (int i = 0; i < getActivity().getSupportFragmentManager().getBackStackEntryCount(); i++) {
-                                getActivity().getSupportFragmentManager().popBackStack();
-                            }
-                            getActivity().finish();
-                            startActivity(intent);
+                            drawProfile(view);
+                        } else {
+                            Log.d(TAG, "Couldn't set new username");
+                            Toast.makeText(getContext(), "Something went wrong.", Toast.LENGTH_SHORT);
                         }
                     }
                 });
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
+
 
     public void sendResetPasswordMail(View view) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -147,6 +193,8 @@ public class ProfileFragment extends Fragment {
                     }
                 });
             }
+        } else {
+            // todo: check if registered with mail and reauthenticate
         }
     }
 }
