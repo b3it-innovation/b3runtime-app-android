0573b22b23344b2c24721ee5908a9f608d960672
William Jelvin <william.jelvin@gmail.com>
2019-11-13 16:22:11 +0100

    continue button now disabled if no current track

--- app/src/main/java/com/b3/development/b3runtime/ui/home/HomeFragment.java
+++ app/src/main/java/com/b3/development/b3runtime/ui/home/HomeFragment.java
@@ -1,20 +1,24 @@
 package com.b3.development.b3runtime.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.b3.development.b3runtime.R;
 import com.b3.development.b3runtime.base.BaseFragment;
-import com.b3.development.b3runtime.ui.map.MapsActivity;
+import com.b3.development.b3runtime.data.repository.checkpoint.CheckpointRepository;
+import com.b3.development.b3runtime.data.repository.useraccount.UserAccountRepository;
+
+import static org.koin.java.KoinJavaComponent.get;
 
 public class HomeFragment extends BaseFragment {
     public static final String TAG = HomeFragment.class.getSimpleName();
     private static final int layoutId = R.layout.fragment_home;
+    private HomeViewModel homeViewModel;
 
 
     public HomeFragment() {
@@ -34,6 +38,9 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(getActivity(),
+                new HomeViewModelFactory(get(UserAccountRepository.class), get(CheckpointRepository.class)))
+                .get(HomeViewModel.class);
 
         view.findViewById(R.id.competition_button)
                 .setOnClickListener(v -> ((HomeActivity) getActivity()).showCompetitionFragment());
@@ -50,5 +57,8 @@ public class HomeFragment extends BaseFragment {
                         Toast.makeText(getContext(), "You're continued!", Toast.LENGTH_SHORT).show();
                     }
                 });
+        homeViewModel.getTrackUnfinished().observe(getViewLifecycleOwner(), aBool -> {
+            view.findViewById(R.id.continue_button).setEnabled(aBool);
+        });
     }
 }
